2bf5ae7f92f3c2b5e381262cb20b5e5922e74a4b
Guilherme Crozariol <guilherme@guilhermes-macbook-pro.local>
2016-12-14 20:38:44 -0200

    Test.

--- Is It Prime/ViewController.swift
+++ Is It Prime/ViewController.swift
@@ -10,6 +10,65 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var numberTextField: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBAction func isItPrime(_ sender: Any) {
+        
+        if let userEnteredString = numberTextField.text {
+            
+            let userEnteredInt = Int(userEnteredString)
+            
+            if let number = userEnteredInt {
+                
+                var isPrime = true
+                
+                if number == 1 {
+                    
+                    isPrime = false
+                    
+                }
+                
+                if number == 2 || number == 3 {
+                    
+                    isPrime = true
+                    
+                }
+                
+                var i = 2
+                
+                while i < number {
+                    
+                    if number % i == 0 {
+                        
+                        isPrime = false
+                        
+                    }
+                    
+                    i += 1
+                    
+                }
+                
+                if isPrime {
+                    
+                    resultLabel.text = "The number \(number) is prime."
+                    
+                } else {
+                    
+                    resultLabel.text = "The number \(number) is not prime."
+                    
+                }
+                
+            } else {
+                
+                resultLabel.text = "Enter a positive whole number."
+                
+            }
+            
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +78,5 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
